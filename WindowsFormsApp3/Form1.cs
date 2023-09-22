28bc1956d3767700a573dc21f2f9e3774fa95a1f
Антон <@DESKTOP-CPLDV61>
2023-09-22 10:09:05 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+
+namespace TestNotifyPropertyChangedCS
+{
+   
+    public partial class Form1 : Form
+    {
+        
+        private Button changeItemBtn = new Button();
+
+       
+        private DataGridView customersDataGridView = new DataGridView();
+
+        private BindingSource customersBindingSource = new BindingSource();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+           
+            this.changeItemBtn.Text = "Change Item";
+            this.changeItemBtn.Dock = DockStyle.Bottom;
+            this.changeItemBtn.Click +=
+                new EventHandler(changeItemBtn_Click);
+            this.Controls.Add(this.changeItemBtn);
+
+            
+            customersDataGridView.Dock = DockStyle.Top;
+            this.Controls.Add(customersDataGridView);
+
+            this.Size = new Size(400, 200);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            BindingList<DemoCustomer> customerList = new BindingList<DemoCustomer>();
+            customerList.Add(DemoCustomer.CreateNewCustomer());
+            customerList.Add(DemoCustomer.CreateNewCustomer());
+            customerList.Add(DemoCustomer.CreateNewCustomer());
+
+            
+            this.customersBindingSource.DataSource = customerList;
+
+
+            this.customersDataGridView.DataSource =
+                this.customersBindingSource;
+        }
+
+       
+        void changeItemBtn_Click(object sender, EventArgs e)
+        {
+       
+            BindingList<DemoCustomer> customerList =
+                this.customersBindingSource.DataSource as BindingList<DemoCustomer>;
+
+           
+            customerList[0].CustomerName = "Tailspin Toys";
+            customerList[0].PhoneNumber = "(708)555-0150";
+        }
+    }
+
+ 
+    public class DemoCustomer : INotifyPropertyChanged
+    {
+        
+        private Guid idValue = Guid.NewGuid();
+        private string customerNameValue = String.Empty;
+        private string phoneNumberValue = String.Empty;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        
+        private DemoCustomer()
+        {
+            customerNameValue = "Customer";
+            phoneNumberValue = "(312)555-0100";
+        }
+
+      
+        public static DemoCustomer CreateNewCustomer()
+        {
+            return new DemoCustomer();
+        }
+
+        
+        public Guid ID
+        {
+            get
+            {
+                return this.idValue;
+            }
+        }
+
+        public string CustomerName
+        {
+            get
+            {
+                return this.customerNameValue;
+            }
+
+            set
+            {
+                if (value != this.customerNameValue)
+                {
+                    this.customerNameValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get
+            {
+                return this.phoneNumberValue;
+            }
+
+            set
+            {
+                if (value != this.phoneNumberValue)
+                {
+                    this.phoneNumberValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+    }
+}
